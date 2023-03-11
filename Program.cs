082977b3ef98841778f9b72b03494a3dd25f610d
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-10 13:29:30 -1200

    Welcome page

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparisionComputationProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Computation Problems");
+        }
+    }
+}
